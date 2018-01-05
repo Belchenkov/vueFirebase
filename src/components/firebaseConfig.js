e8011a7cf32128e8cc586ced5c296dfb30452ccf
Aleksey Belchenkov <u608110@gmail.com>
2018-01-05 11:11:55 +0430

    Add Navbar

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyD5fI5L0mDLGgYBHdeHMK9Kvl1N7Io2sTs",
+  authDomain: "vuejsfirestore.firebaseapp.com",
+  databaseURL: "https://vuejsfirestore.firebaseio.com",
+  projectId: "vuejsfirestore",
+  storageBucket: "vuejsfirestore.appspot.com",
+  messagingSenderId: "925828188946"
+};
